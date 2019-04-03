86327f7c25999f4ed6633b91da5ca347cc4155e6
oyj <you@example.com>
2019-04-04 03:23:22 +0900

    modified scp option

--- admjoin.sh
+++ admjoin.sh
@@ -13,7 +13,7 @@ if [[ $NETCHECK ]]
 then
   echo -e "\e[33m Seems like this node already joined \e[0m"
 else
-  sudo scp -o StrictHostKeyChecking=no -i /home/vagrant/.ssh/id_rsa 10.1.0.2:/vagrant/admin_init.log /vagrant/	 
+  sudo scp -o StrictHostKeyChecking=no -i /home/vagrant/.ssh/id_rsa vagrant@10.1.0.2:/vagrant/admin_init.log /vagrant/
   echo -e "\e[36m Node will join with master 10.1.0.2 \e[0m"
   #JOINC=$(ssh -o StrictHostKeyChecking=no -i /home/vagrant/.ssh/id_rsa vagrant@10.1.0.2 'cat /vagrant/admin_init.log | grep "kubeadm join 10.1.0.2:6443"')
   JOINC=$(cat /vagrant/admin_init.log | tail -2 | sed 's/\\//g')
